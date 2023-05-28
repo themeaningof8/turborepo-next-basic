35005585b85eaeca8796d4d491faedacf1571235
Ueda Kazki <themeaningof8@gmail.com>
2023-05-29 00:49:56 +0900

    token studio

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,22 +1,14 @@
 import type { Config } from "tailwindcss";
-import colors from "tailwindcss/colors";
 import Typography from "@tailwindcss/typography";
+import { filterTokensByType } from "./fns";
+import * as tokens from "./tokens/light.json"
+
+const colors = filterTokensByType('color', tokens)
 
 export default {
   content: ["./app/**/*.{ts,tsx}"],
   theme: {
-    textColor: {
-      primary: "#0c0a09",
-      secondary: "#44403c",
-      muted: "#737373",
-    },
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      black: "#000",
-      white: "#fff",
-      gray: colors.stone,
-    },
+    ...colors,
     fontFamily: {
       sans: [
         `"system-ui", "-apple-system", "BlinkMacSystemFont", "Hiragino Kaku Gothic ProN", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"`,
