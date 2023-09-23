8d8447d65c5b2643b1a1768f3be86135384a3808
Ueda Kazki <themeaningof8@gmail.com>
2023-09-23 16:09:20 +0900

    remove next-config experimental

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  experimental: {
-    appDir: true,
-  },
   reactStrictMode: true,
   swcMinify: true,
   transpilePackages: ['@themeaningof8/ui'],
