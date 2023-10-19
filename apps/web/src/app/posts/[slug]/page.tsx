4874f848a12e99c1d557e6ae38e6b8bf96c4a4a6
Ueda Kazki <themeaningof8@gmail.com>
2023-10-19 11:33:32 +0900

    postをconsole.logしてみる

--- apps/web/src/app/posts/[slug]/page.tsx
+++ apps/web/src/app/posts/[slug]/page.tsx
@@ -1,18 +1,16 @@
 import keystaticConfig from '@/keystatic.config';
 import { createReader } from '@keystatic/core/reader';
 import { DocumentRenderer } from '@keystatic/core/renderer';
-import { Suspense } from 'react';
 
-const reader = createReader(process.cwd(), keystaticConfig);
+const reader = createReader(process.cwd(), keystaticConfig)
 
 export default async function Post({ params }) {
-  const post = await reader.collections.posts.read(params.slug);
+  const post = await reader.collections.posts.read(params.slug)
+  console.log(post)
   return (
     <>
-      <h1>{post.title}</h1>
-      <Suspense>
-        <DocumentRenderer document={await post.content()} />
-      </Suspense>
+      {/* <h1>{post.title}</h1>
+      <DocumentRenderer document={await post.content()} /> */}
     </>
   )
 }
