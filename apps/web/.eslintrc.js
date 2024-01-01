079d1ba957ec5801707142782f14d7b553eb5879
Kazki Ueda <themeaningof8@gmail.com>
2024-01-01 23:17:20 +0900

    Add biome.json files and update package.json scripts

--- apps/web/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ['@themeaningof8/eslint-config-custom'],
-  parserOptions: {
-    babelOptions: {
-      presets: [require.resolve('next/babel')]
-    }
-  },
-  root: true
-}
--- packages/design-token/.eslintrc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  // This tells ESLint to load the config from the package `@themeaningof8/eslint-config-custom`
-  extends: ['@themeaningof8/eslint-config-custom'],
-  ignorePatterns: ['node_modules', 'tokens']
-}
