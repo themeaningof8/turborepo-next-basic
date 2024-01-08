67d735979b0fde6283050d34fad798f11314e41b
Kazki Ueda <themeaningof8@gmail.com>
2024-01-08 15:29:52 +0900

    Update dependencies and global styles

---
+++ apps/web/src/app/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: 'Kazki Ueda',
+  url: 'https://www.kazkiueda.com',
+  ogImage: 'https://www.kazkiueda.com/og.jpg',
+  description:
+    'Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.',
+  links: {
+    twitter: 'https://twitter.com/KazkiUeda',
+    github: 'https://github.com/themeaningof8'
+  }
+}
+
+export type SiteConfig = typeof siteConfig
