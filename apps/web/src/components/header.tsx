7684dfc4c73348684a79abbbc1641094948abf2a
Kazki Ueda <themeaningof8@gmail.com>
2024-01-21 16:53:43 +0900

    Add new paths to tsconfig.json and package.json, and update imports in button.tsx and header.tsx

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,13 +1,19 @@
-import { Button } from '@/src/components/ui/button'
+import Link from 'next/link'
+
+import { Button } from '@components/ui/button'
+import { CommandMenu } from '@components/commandMenu'
 import { siteConfig } from '@/src/config/site'
 
 export default function Header() {
   return (
     <header className="flex items-center gap-12 px-5 h-16 border border-slate-100">
       <div>{siteConfig.name}</div>
-      <nav className="flex items-center">
-        <Button size="sm">Blog</Button>
+      <nav className="flex flex-1 items-center">
+        <Button size="sm" variant="ghost" asChild>
+          <Link href="/posts">Posts</Link>
+        </Button>
       </nav>
+      <CommandMenu />
     </header>
   )
 }
