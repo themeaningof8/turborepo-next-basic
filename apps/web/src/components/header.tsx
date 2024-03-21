a163956ed43a964bd5d8a874543e65f0862812ae
Kazki Ueda <themeaningof8@gmail.com>
2024-03-21 22:40:41 +0900

    Add logo to header component

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,15 +1,19 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 import { siteConfig } from '@/src/config/site'
 import { CommandMenu } from '@components/command-menu'
 import { ModeToggle } from '@components/mode-toggle'
 import { Button } from '@themeaningof8/ui/components/button'
+import Logo from '../../public/logo.svg'
 
 export default function Header() {
   return (
-    <header className="flex h-12 items-center gap-12 border-border border-b px-5">
+    <header className="flex h-14 items-center gap-12 px-5">
       <Button size="sm" variant="ghost" asChild>
-        <Link href="/">{siteConfig.name}</Link>
+        <Link href="/">
+          <Logo className="h-4 fill-current dark:fill-background" />
+        </Link>
       </Button>
       <nav className="flex-1 flex items-center">
         <Button size="sm" variant="ghost" asChild>
