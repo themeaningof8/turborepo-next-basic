3d9ff88a01b1a8397a5b9fc1a840846800c9822e
Kazki Ueda <themeaningof8@gmail.com>
2024-03-23 23:05:19 +0900

    Add UI components and configuration files

---
+++ packages/ui/.storybook/main.ts
@@ -0,0 +1,39 @@
+import { dirname, join, resolve } from 'path'
+import type { StorybookConfig } from '@storybook/react-vite'
+import { mergeConfig } from 'vite'
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, 'package.json')))
+}
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/addon-actions'),
+    getAbsolutePath('@storybook/addon-interactions')
+  ],
+  framework: {
+    name: getAbsolutePath('@storybook/react-vite'),
+    options: {}
+  },
+  docs: {
+    autodocs: 'tag'
+  },
+  viteFinal: (viteConfig) =>
+    mergeConfig(viteConfig, {
+      define: { 'process.env': {} },
+      resolve: {
+        alias: {
+          '@ui': resolve(__dirname, '../src')
+        }
+      }
+    })
+}
+
+export default config
