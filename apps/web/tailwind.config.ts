49ff4685b449a61570e68a2ad77b60dac63898ab
Kazki Ueda <themeaningof8@gmail.com>
2024-04-13 23:09:34 +0900

    Update pnpm-workspace.yaml, tailwind.config.ts, and package.json

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -4,7 +4,7 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
   content: [
     './src/**/*.{ts,tsx}',
-    './node_modules/@themeaningof8/ui/**/*.{ts,tsx}'
+    '../../node_modules/@themeaningof8/ui/**/*.{ts,tsx}'
   ],
   darkMode: ['class'],
   presets: [Presets]
